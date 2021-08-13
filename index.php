bed450a77be203f453c31cfc7ca1ae5cb3ef4a15
imlohiya <kinglohiya21@gmail.com>
2021-08-13 11:43:38 +0530

    task completed successfully

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+   <title>Task</title>
+   <!-- Google Fonts -->
+   <link href='http://fonts.googleapis.com/css?family=Raleway:400,800,500,600,300,700' rel='stylesheet' type='text/css'>
+   <!-- bootstrap -->
+   <link href="css/bootstrap.css" rel="stylesheet">
+   <!-- Styles CSS-->
+   <link href="css/style.css" rel="stylesheet">
+   
+</head>
+
+<body class="box-layout">
+   <!-- Page Loader -->
+   <div class="page-loader">
+      <div class="loader">
+         <span class="cssload-loading"></span>
+      </div>
+   </div>
+   <!-- End Page Loader -->
+   <div class="inner-conterner">
+      <!-- inner-conterner -->
+      <header class="site-header">
+         <div class="header-inner">
+            <!-- navigation panel -->
+            <div class="container" id="home">
+               <div class="row">
+                  <div class="header-table col-md-12">
+                     <div class="brand">
+                        <a href="index.php">
+                           <h3>ML Creativity</h3>
+                        </a>
+                     </div>
+                     <nav id="nav-wrap" class="main-nav">
+                        <div id="mobnav-btn"> </div>
+                        <ul class="sf-menu">
+                           <li class="current">
+                              <a href="index.php">Home</a>
+                           </li>
+                           <li>
+                              <a href="#about">About Us</a>                             
+                           </li>
+                           <li>
+                              <a href="#career">Careers</a>
+                           </li>
+                        </ul>
+                     </nav>
+                  </div>
+               </div>
+               <!-- End navigation panel -->
+            </div>
+            <!-- End navigation panel -->
+         </div>
+      </header>
+      <!-- Slider -->
+      <div class="slider-wrapper">
+         <div class="fr-slider">
+            <div class="fs_loader"></div>
+            <div class="slide">
+               <img data-fixed class="slide-bg" src="images/features_bg.jpg" alt="Slide">
+            </div>
+            <div class="slide">
+               <img data-fixed class="slide-bg" src="images/slide001.jpg" alt="Slide">
+            </div>
+            <div class="slide">
+               <img data-fixed class="slide-bg" src="images/slide3.jpg" alt="Slide">
+            </div>
+         </div>
+      </div>
+      <!-- End Slider -->
+      <!-- About Section -->
+      <div class="container-about" id="about">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <h3 class="title-un">ABOUT US</h3>
+                  <div class="title-un-icon"><i class="fa ion-ios-flame-outline"></i></div>
+                  
+                  <div class="container">
+                     <br>
+                      <div class="h4 text-center mb-4 title">Professional Skills</div>     
+                  </div>
+
+                  <div class="card" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                     <div class="card-body">
+                       <div class="row">
+                         <div class="col-md-6">
+                           <div class="progress-container progress-primary"><span class="progress-badge">HTML</span>
+                             <div class="progress">
+                               <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 93%;"></div><span class="progress-value">93%</span>
+                             </div>
+                           </div>
+                         </div>
+                         <div class="col-md-6">
+                           <div class="progress-container progress-primary"><span class="progress-badge">CSS</span>
+                             <div class="progress">
+                               <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 85%;"></div><span class="progress-value">85%</span>
+                             </div>
+                           </div>
+                         </div>
+                       </div>
+                       <div class="row">
+                         <div class="col-md-6">
+                           <div class="progress-container progress-primary"><span class="progress-badge">JavaScript</span>
+                             <div class="progress">
+                               <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div><span class="progress-value">60%</span>
+                             </div>
+                           </div>
+                         </div>
+                         <div class="col-md-6">
+                           <div class="progress-container progress-primary"><span class="progress-badge">PHP</span>
+                             <div class="progress">
+                               <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 78%;"></div><span class="progress-value">78%</span>
+                             </div>
+                           </div>
+                         </div>
+                       </div>
+                       <div class="row">
+                         <div class="col-md-6">
+                           <div class="progress-container progress-primary"><span class="progress-badge">Bootstrap</span>
+                             <div class="progress">
+                               <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 85%;"></div><span class="progress-value">85%</span>
+                             </div>
+                           </div>
+                         </div>
+                         <div class="col-md-6">
+                           <div class="progress-container progress-primary"><span class="progress-badge">Angular JS</span>
+                             <div class="progress">
+                               <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 77%;"></div><span class="progress-value">77%</span>
+                             </div>
+                           </div>
+                         </div>
+                       </div>
+                     </div>
+                  </div>
+                  <br>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-4">
+                  <!-- Service item -->
+                  <div class="service-box-sb wow fadeInUp">
+                     <div class="service-img">
+                        <img src="images/sas.jpg" alt="Enfold">
+                     </div>
+                     <div class="service-info">
+                        <h5>UNIQUE FEATURES</h5>
+                        <div class="desc">
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis sapien sit amet arcu ornare, non commodo ligula consectetur adipiscing elit.
+                           <a class="see-more" href="#about">
+                        See More &rarr;
+                        <div class="line"></div>
+                      </a>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- End Service item -->
+               </div>
+               <div class="col-md-4">
+                  <!-- Service item -->
+                  <div class="service-box-sb wow fadeInUp" data-wow-delay=".2s">
+                     <div class="service-img">
+                        <img src="images/sasa.jpg" alt="Enfold">
+                     </div>
+                     <div class="service-info">
+                        <h5>RESPONSIVE LAYOUT</h5>
+                        <div class="desc">
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis sapien sit amet arcu ornare, non commodo ligula consectetur adipiscing elit.
+                           <a class="see-more" href="#">
+                        See More &rarr;
+                        <div class="line"></div>
+                      </a>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- End Service item -->
+               </div>
+               <div class="col-md-4">
+                  <!-- Service item -->
+                  <div class="service-box-sb wow fadeInUp" data-wow-delay=".4s">
+                     <div class="service-img">
+                        <img src="images/sasaa.jpg" alt="Enfold">
+                     </div>
+                     <div class="service-info">
+                        <h5>PAGE BUILDER</h5>
+                        <div class="desc">
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis sapien sit amet arcu ornare, non commodo ligula consectetur adipiscing elit.
+                           <a class="see-more" href="#">See More &rarr;<div class="line"></div></a>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- End Service item -->
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- End About Section -->
+ 
+      <!-- Testimonials Section -->
+      <div class="testimonials-container" id="career">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 wow fadeInUp">
+                  <div class="title-un-icon">
+                     <h3 class="title-un">CAREERS</h3>
+                  </div>
+                  <div id="owl-slide" class="testimonials owl-carousel">
+                     
+                     <div class="testimonial">
+                        <p>
+                           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus quia quod, maxime temporibus facilis non libero blanditiis esse laudantium voluptatibus dolorum. Enim inventore perspiciatis natus ducimus iure dignissimos officia sunt autem, accusamus expedita mollitia ut nisi nostrum sint porro aliquam error voluptates ea nulla officiis aut, laudantium. Debitis accusantium nam velit quisquam eius minus magni itaque excepturi molestias, id dolor harum nostrum tenetur deserunt eligendi! Pariatur sequi consectetur harum eveniet totam nostrum ducimus voluptatum animi, fugiat quam, odit sed facilis, qui veritatis iusto explicabo reiciendis? Debitis asperiores alias esse culpa at eius rerum tempore hic illum repellat placeat quae, eligendi quis sint corporis perferendis deleniti maxime corrupti reiciendis explicabo aliquid quas voluptate iure? Quis, ipsum, veniam? Voluptatem, itaque numquam impedit eligendi repudiandae sequi veritatis fugiat nihil, voluptatibus quod molestiae minima voluptate mollitia reprehenderit, velit cum repellendus labore aut sit asperiores nulla possimus alias magnam commodi natus. Iusto perferendis, illum hic vero, aspernatur nostrum repudiandae nulla et doloremque voluptates quisquam ab atque provident voluptas blanditiis quo. Nostrum, quis omnis porro. Voluptatibus est tempora, amet dolorem, autem possimus dignissimos provident quasi sapiente nobis culpa veniam odio, expedita a? Facilis eaque quod sed non tenetur atque similique sapiente cumque, quia delectus laborum quibusdam aut eveniet blanditiis? Ducimus ea, sapiente provident impedit expedita repellat optio rem vel ratione laboriosam, officiis molestias! Ipsam, quisquam, architecto. Ullam ad voluptatum reiciendis, quibusdam perferendis tempore aperiam dolor quidem ab, at, sunt. Quisquam ad, quas fugit maiores et molestias beatae eligendi. Repudiandae consectetur, quaerat corporis omnis earum praesentium, non.
+                        </p>
+                        
+                     </div>
+                     
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- End Testimonials Section -->
+
+      <!-- Footer area -->
+      <div class="footer-area-container">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-2"></div>
+               <div class="col-md-5">
+                  <div class="footer-widget widget_nav_menu">
+                     <h4 class="footer-widget-title">Get In Touch</h4>
+                     <div> <i class="ion-ios-location-outline"></i> Jalna, Maharashtra
+                        <br>
+                        <i class="ion-ios-telephone-outline"></i> +91 7057036382
+                        <br>
+                        <i class="ion-paper-airplane"></i> <a href="kinglohiya21@gmail.com"> kinglohiya21@gmail.com</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-3">
+                  <div class="footer-widget widget_nav_menu">
+                     <h4 class="footer-widget-title">LINKS</h4>
+                     <ul>
+                        <li class="menu-item"><a href="#home"> HOME </a></li>
+                        <li class="menu-item"><a href="#about"> ABOUT US </a></li>
+                        <li class="menu-item"><a href="#career"> CAREER </a></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-md-2"></div>
+            </div>
+         </div>
+      </div>
+      <!-- End Footer area -->
+      <!-- Footer -->
+      <div class="footer-holder">
+         <div class="container">
+            <footer class="site-footer">
+               <div class="row">
+                  <div class="col-md-6">
+                     <p>Developed by Mahesh Lohiya</p>
+                  </div>
+                  
+               </div>
+            </footer>
+         </div>
+      </div>
+      <!-- End Footer -->
+   </div>
+   <!-- End inner-conterner -->
+   <!-- ================================================== -->
+   <!-- Placed js files at the end of the document so the pages load faster -->
+   <script src="js/jquery.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+   <script src="js/superfish.js"></script>
+   <script src="js/jquery.easing.js"></script>
+   <script src="js/wow.js"></script>
+   <script src="js/jquery.isotope.min.js"></script>
+   <script src="js/owl.carousel.min.js"></script>
+   <script src="js/jquery.magnific-popup.js"></script>
+   <script src="js/jflickrfeed.min.js"></script>
+   <script src="js/jquery.fitvids.js"></script>
+   <script src="js/jquery.fractionslider.min.js"></script>
+   <script src="js/jquery-ui-1.10.4.custom.min.js"></script>
+   <script type="text/javascript" src="js/SmoothScroll.js"></script>
+   <script src="js/main.js"></script>
+</body>
+
+</html>
